35f43084c9ca795103dc2fe80927c12d683466f2
grantlandon <grant.landon4@gmail.com>
2022-10-15 15:57:07 -0400

    adding forms

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from os import path
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
 def find_simiarlies(request):
     return HttpResponse(0.0)
 
