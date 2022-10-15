8f91c78fb4cd85b6b48947424d81c2163a3898ed
alicejmin <90526396+alicejmin@users.noreply.github.com>
2022-10-15 15:08:16 -0400

    janky

--- frontend/SpeechMe/SpeechMeApp.swift
+++ frontend/SpeechMe/SpeechMeApp.swift
@@ -5,15 +5,45 @@
 //  Created by Haruka Masamura on 10/14/22.
 //
 
+//
+//  SpeechMeApp.swift
+//  SpeechMe
+//
+//  Created by Haruka Masamura on 10/14/22.
+//
+
 import SwiftUI
+import UIKit
 
 // MARK: - Section 1
-
 @main
 struct SpeechMeApp: App {
+    @UIApplicationDelegateAdaptor var appDelegate : AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             InputView()
+        }
+    }
+    
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            // Override point for customization after application launch.
+            return true
+        }
+
+        // MARK: UISceneSession Lifecycle
+
+        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        }
+
+        func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+            // Called when the user discards a scene session.
+            // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
         }
     }
 }
