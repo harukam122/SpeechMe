8fc191eba98059e6fb0a6a4dab00a6956ccc6577
Arata Fujii <arata_fujii@brown.edu>
2022-10-15 20:16:03 -0400

    api first draft

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,29 @@
-from requests import request
-from flask import Flask, jsonoify
+from flask import Flask, jsonify, request
 from transcribe import GetScore
 import os
 import base64
 
 app = Flask(__name__)
 
-@app.route('/text', methods=['POST'])
-def text():
-    text = request.text["text"]
+# @app.route('/text', methods=['POST'])
+# def text():
+#     text = request.text["text"]
 
 @app.route('/audio', methods=['POST'])
 def audio():
-    audio_file = request.files["file"]
+    print(request)
+    audio_file = request.json["audio"]
     file_name = "recording.wav"
     wav_file = open("temp.wav", "wb")
     decode_string = base64.b64decode(audio_file)
     wav_file.write(decode_string)
-    audio_file.save(file_name)
 
-    score = GetScore("some word", audio_file, 'b8c4374e49ae4dc2b75626b5b9052d0f')
+    score = GetScore(request.json["text"], audio_file, 'b8c4374e49ae4dc2b75626b5b9052d0f')
 
     os.remove(file_name)
 
     data = {"score" : score}
-    return jsonoify(data)
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=False)
